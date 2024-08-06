b9fe9bb403242455083f597b9e6df20e95cc6a19
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2024-08-06 19:23:56 +0600

    test

--- confiq/db.js
+++ confiq/db.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 const mySqlPool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "srv1267.hstgr.io",
+  user: "u323738017_u_groman",
+  password: "|zG=9Fyfn2",
+  database: "u323738017_grocery_manage",
 });
+// const mySqlPool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 
 module.exports = mySqlPool;
