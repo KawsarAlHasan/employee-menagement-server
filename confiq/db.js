9a68fd178ff534e08f213cf50d3f9196546728c7
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2024-08-06 20:33:54 +0600

    some change

--- confiq/db.js
+++ confiq/db.js
@@ -1,16 +1,10 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 const mySqlPool = mysql.createPool({
-  host: "srv1267.hstgr.io",
-  user: "u323738017_u_groman",
-  password: "|zG=9Fyfn2",
-  database: "u323738017_grocery_manage",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
-// const mySqlPool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
 
 module.exports = mySqlPool;
--- index.js
+++ index.js
@@ -29,12 +29,6 @@ app.use("/api/v1/food-cost", require("./routes/foodCostRoute"));
 app.use("/api/v1/profit", require("./routes/lossAndProfitRoute"));
 app.use("/api/v1/general-setting", require("./routes/generalSettingRoute"));
 app.use("/api/v1/notification", require("./routes/notificationsRoute"));
-// 404 Not Found middleware
-app.use((req, res, next) => {
-  res.status(404).json({
-    error: "You have hit the wrong route",
-  });
-});
 
 const port = process.env.PORT || 5000;
 
@@ -54,4 +48,11 @@ mySqlPool
 
 app.get("/", (req, res) => {
   res.status(200).send("Employee Management Server is working");
+});
+
+// 404 Not Found middleware
+app.use("*", (req, res, next) => {
+  res.status(404).json({
+    error: "You have hit the wrong route",
+  });
 });
